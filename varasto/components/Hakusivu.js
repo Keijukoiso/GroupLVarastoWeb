f2e0ea588c9e89c7cef8f311c352dc0cb03ef7a3
Keijukoiso <meripakk@gmail.com>
2021-03-03 08:07:48 +0200

    esimerkit omaan kansioon, lisätty työlle alkua

---
+++ varasto/components/Hakusivu.js
@@ -0,0 +1,12 @@
+import Haku from '../components/Haku.js';
+import Catalog from '../components/Catalog.js';
+
+export default function Hakusivu () {
+	return (
+        <div>
+            <Haku />
+            <Catalog />
+        </div>
+		
+);
+}  
